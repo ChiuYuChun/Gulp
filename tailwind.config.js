644fa87b237618fc66f4d4c0b6a61da7c5ec3227
ChiuYuChun <qwe3322568@gmail.com>
2022-07-21 13:03:49 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{html,ejs}"],
+  theme: { 
+    container: {
+    center: true, 
+    padding: "12px"
+  },
+    extend: {},
+  },
+  plugins: [],
+}
